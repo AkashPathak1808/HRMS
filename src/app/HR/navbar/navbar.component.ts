08cc875d2bc41bda1d7f877db327a44c8e11535a
iamsagargg <ghritlaharesagar0226@gmail.com>
2024-03-13 16:13:51 +0530

    Navbar

--- src/app/HR/navbar/navbar.component.ts
+++ src/app/HR/navbar/navbar.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-
+  dashboard = 'assets/DashboardImg/dashboard.svg';
+  allEmp = 'assets/DashboardImg/allEmp.svg';
+  clients = 'assets/DashboardImg/clients.svg';
+  attendence = 'assets/DashboardImg/attendence.svg';
+  projects = 'assets/DashboardImg/pro.svg';
+  jobs = 'assets/DashboardImg/jobs.svg';
+  tasks = 'assets/DashboardImg/tasks.svg';
+  candidate = 'assets/DashboardImg/candidate.svg';
+  payroll = 'assets/DashboardImg/payroll.svg';
+  mgmt = 'assets/DashboardImg/mgmt.svg';
+  leaves = 'assets/DashboardImg/leaves.svg';
+  hodidays = 'assets/DashboardImg/hodidays.svg';
+  settings = 'assets/DashboardImg/settings.svg';
+  logout = 'assets/DashboardImg/logout.svg';
+  railworldLogo = 'assets/DashboardImg/railworldLogo.svg';
 }
