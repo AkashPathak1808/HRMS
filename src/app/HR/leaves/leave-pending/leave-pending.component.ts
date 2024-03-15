a6611c66683608683036244fd7f87d068bb8df66
Ankana-Chatterjee <90030694+Ankana-Chatterjee@users.noreply.github.com>
2024-03-15 14:34:06 +0530

    created leave component

---
+++ src/app/HR/leaves/leave-pending/leave-pending.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-leave-pending',
+  templateUrl: './leave-pending.component.html',
+  styleUrls: ['./leave-pending.component.css']
+})
+export class LeavePendingComponent {
+
+}
