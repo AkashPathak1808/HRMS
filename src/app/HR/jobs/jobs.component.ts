327649d1886f22da494831f1c2d6f301aa310cce
AkashPathak1808 <akashpathak180801@gmail.com>
2024-03-28 17:10:41 +0530

    Worked on Jobs components & changes on  the navbar

--- src/app/HR/jobs/jobs.component.ts
+++ src/app/HR/jobs/jobs.component.ts
@@ -6,5 +6,84 @@ import { Component } from '@angular/core';
   styleUrls: ['./jobs.component.css']
 })
 export class JobsComponent {
+  user = "assets/CandidateImg/user.png";
+  case = "assets/Jobs/briefcase.svg";
 
+  activeJobs = [
+    {
+      title: "UI/UX Designer",
+      designation: 'Design',
+      department: 'Design',
+      time: 'Full Time',
+      remote: true,
+      location: 'California, USA',
+      ctc: '$3600/Month'
+    },
+    {
+      title: "UI/UX Designer",
+      designation: 'Design',
+      department: 'Design',
+      time: 'Full Time',
+      remote: false,
+      location: 'California, USA',
+      ctc: '$3600/Month'
+    },
+    {
+      title: "UI/UX Designer",
+      designation: 'Design',
+      department: 'Design',
+      time: 'Full Time',
+      remote: true,
+      location: 'California, USA',
+      ctc: '$3600/Month'
+    },
+    {
+      title: "UI/UX Designer",
+      designation: 'Design',
+      department: 'Design',
+      time: 'Full Time',
+      remote: false,
+      location: 'California, USA',
+      ctc: '$3600/Month'
+    }
+  ]
+
+  inactiveJobs = [
+    {
+      title: "UI/UX Designer",
+      designation: 'Design',
+      department: 'Design',
+      time: 'Full Time',
+      remote: true,
+      location: 'California, USA',
+      ctc: '$3600/Month'
+    },
+    {
+      title: "UI/UX Designer",
+      designation: 'Design',
+      department: 'Design',
+      time: 'Full Time',
+      remote: false,
+      location: 'California, USA',
+      ctc: '$3600/Month'
+    },
+    {
+      title: "UI/UX Designer",
+      designation: 'Design',
+      department: 'Design',
+      time: 'Full Time',
+      remote: true,
+      location: 'California, USA',
+      ctc: '$3600/Month'
+    },
+    {
+      title: "UI/UX Designer",
+      designation: 'Design',
+      department: 'Design',
+      time: 'Full Time',
+      remote: false,
+      location: 'California, USA',
+      ctc: '$3600/Month'
+    }
+  ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -34,7 +34,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
-
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -74,7 +74,8 @@ import { MatCardModule } from '@angular/material/card';
     MatFormFieldModule,
     MatInputModule,
     ReactiveFormsModule,
-    MatCardModule
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
